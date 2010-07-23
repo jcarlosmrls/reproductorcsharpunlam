fae47077b14342f890461b95fbb8973ff528b03b
fabionegri1990@gmail.com <fabionegri1990@gmail.com@e66291ac-1e80-1330-76bf-9038f0f03be7>
2010-07-23 17:11:20 +0000

    Primera aproximacion a un Login

---
+++ trunk/Reproductor/Login.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Reproductor
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.botonLogin = new System.Windows.Forms.Button();
+            this.botonCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botonLogin
+            // 
+            this.botonLogin.Location = new System.Drawing.Point(98, 113);
+            this.botonLogin.Name = "botonLogin";
+            this.botonLogin.Size = new System.Drawing.Size(76, 28);
+            this.botonLogin.TabIndex = 0;
+            this.botonLogin.Text = "Login";
+            this.botonLogin.UseVisualStyleBackColor = true;
+            this.botonLogin.Click += new System.EventHandler(this.botonLogin_Click);
+            // 
+            // botonCancelar
+            // 
+            this.botonCancelar.Location = new System.Drawing.Point(180, 113);
+            this.botonCancelar.Name = "botonCancelar";
+            this.botonCancelar.Size = new System.Drawing.Size(76, 28);
+            this.botonCancelar.TabIndex = 1;
+            this.botonCancelar.Text = "Cancelar";
+            this.botonCancelar.UseVisualStyleBackColor = true;
+            this.botonCancelar.Click += new System.EventHandler(this.botonCancelar_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(269, 153);
+            this.ControlBox = false;
+            this.Controls.Add(this.botonCancelar);
+            this.Controls.Add(this.botonLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonLogin;
+        private System.Windows.Forms.Button botonCancelar;
+    }
+}
