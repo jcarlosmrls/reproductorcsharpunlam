508eccdd79acfd932bb0e6e328c5b035747ac5e8
fabionegri1990 <fabionegri1990@gmail.com>
2010-09-03 02:50:23 +0000

    Primer intento en reproducir canciones :P

---
+++ Reproductor/Player.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reproductor
+{
+    class Player
+    {
+        private string Pcommand;
+	    private bool isOpen;
+		private bool isPlaying = false;
+		
+		[DllImport("winmm.dll")]
+		private static extern long mciSendString(string strCommand,StringBuilder strReturn,int iReturnLength, IntPtr hwndCallback);
+		
+		public Player()
+		{
+		}
+		
+		public bool Reproduciendo()
+		{
+			return isPlaying;				
+		}
+		
+		public void Close()
+		{
+			Pcommand = "close MediaFile";
+			mciSendString(Pcommand, null, 0, IntPtr.Zero);
+		      	isOpen=false;
+			isPlaying=false;
+		}
+
+		public void Open(string sFileName)
+			{
+				Pcommand = "open \"" + sFileName + "\" type mpegvideo alias MediaFile";
+				mciSendString(Pcommand, null, 0, IntPtr.Zero);
+				isOpen = true;
+			}
+
+		public void Play(bool loop)
+		{
+			if(isOpen)
+				if(isPlaying)
+				{
+					Pcommand = "pause MediaFile";
+					mciSendString(Pcommand, null, 0, IntPtr.Zero);
+					isPlaying = false;
+				}				
+				else
+				{
+					isPlaying = true;
+					Pcommand = "play MediaFile";
+					if (loop)
+						Pcommand += " REPEAT";
+						mciSendString(Pcommand, null, 0, IntPtr.Zero);
+				}
+		}
+		
+		public void Pause()
+		{
+			if(isOpen)
+				if(isPlaying)
+				{
+					Pcommand = "pause MediaFile";
+					mciSendString(Pcommand, null, 0, IntPtr.Zero);
+					isPlaying = false;
+				}
+				else
+				{
+					isPlaying = true;
+					mciSendString(Pcommand, null, 0, IntPtr.Zero);
+				}
+				
+		}
+
+    }
+}
