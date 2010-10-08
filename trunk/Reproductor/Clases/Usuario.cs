b4ff1c2f6a8d60ac4f87437344b379cc21864286
fabionegri1990@gmail.com <fabionegri1990@gmail.com@e66291ac-1e80-1330-76bf-9038f0f03be7>
2010-10-08 16:44:01 +0000

    Nueva clase 'Usuario', y acomodadas las clases dentro del proyecto

---
+++ trunk/Reproductor/Clases/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reproductor.Clases
+{
+    class Usuario
+    {
+        private string ID;
+
+        public string Id
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+    }
+}
