5a9b85d262d1a6b78029fa194f0aeb1da428db56
fabionegri1990 <fabionegri1990@gmail.com>
2010-10-15 20:26:35 +0000

    Clase Configuracion. Cada vez que se cambie el skin o el perfil, se debe modificar la variable "private Configuracion config" para que cuando se cierre el programa o se cambie de usuario, guarde estos cambios.

---
+++ Reproductor/Clases/Configuracion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Reproductor
+{
+    public class Configuracion
+    {
+        private string perfil;
+        private string skin;
+        private string path;
+
+        public Configuracion()
+        {
+            path = Directory.GetCurrentDirectory();
+            path = path.Remove(path.LastIndexOf("\\"));
+            path = path.Remove(path.LastIndexOf("\\"));
+            path = path.Remove(path.LastIndexOf("\\"));
+            path += "\\Base_Reproductor.mdb";
+        }   
+
+        public string Perfil
+        {
+            get
+            {
+                return perfil;
+            }
+            set
+            {
+                perfil = value;
+            }
+        }
+
+        public string Skin
+        {
+            get
+            {
+                return skin;
+            }
+          set
+            {
+                skin = value;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+        }
+    }
+}
