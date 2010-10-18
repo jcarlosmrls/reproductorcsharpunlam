059a527f291c9ffb45af33fcce41a6aa9843a548
fabionegri1990@gmail.com <fabionegri1990@gmail.com@e66291ac-1e80-1330-76bf-9038f0f03be7>
2010-10-18 23:18:43 +0000

    Agregados: * Nueva propiedad de la clase Cancion, que devuelve el numero de la cancion en el album * Nuevo form IngresarNombreDeLista, que permite ingresar el nombre para una nueva lista de reproduccion * Nuevo metodo en la clase BaseDeDatos, para agregar futuras canciones de acuerdo a la lista de reproduccion

---
+++ trunk/Reproductor/Forms/IngresarNombreDeLista.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Reproductor
+{
+    partial class IngresarNombreDeLista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonAceptar = new System.Windows.Forms.Button();
+            this.botonCancelar = new System.Windows.Forms.Button();
+            this.groupBoxLista = new System.Windows.Forms.GroupBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.groupBoxLista.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // botonAceptar
+            // 
+            this.botonAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.botonAceptar.Location = new System.Drawing.Point(47, 80);
+            this.botonAceptar.Name = "botonAceptar";
+            this.botonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.botonAceptar.TabIndex = 0;
+            this.botonAceptar.Text = "Aceptar";
+            this.botonAceptar.UseVisualStyleBackColor = true;
+            this.botonAceptar.Click += new System.EventHandler(this.botonAceptar_Click);
+            // 
+            // botonCancelar
+            // 
+            this.botonCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.botonCancelar.Location = new System.Drawing.Point(143, 80);
+            this.botonCancelar.Name = "botonCancelar";
+            this.botonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.botonCancelar.TabIndex = 1;
+            this.botonCancelar.Text = "Cancelar";
+            this.botonCancelar.UseVisualStyleBackColor = true;
+            this.botonCancelar.Click += new System.EventHandler(this.botonCancelar_Click);
+            // 
+            // groupBoxLista
+            // 
+            this.groupBoxLista.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxLista.Controls.Add(this.txtNombre);
+            this.groupBoxLista.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxLista.Name = "groupBoxLista";
+            this.groupBoxLista.Size = new System.Drawing.Size(246, 62);
+            this.groupBoxLista.TabIndex = 2;
+            this.groupBoxLista.TabStop = false;
+            this.groupBoxLista.Text = "Ingrese el nombre de la nueva Lista:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNombre.Location = new System.Drawing.Point(6, 26);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(234, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // IngresarNombreDeLista
+            // 
+            this.AcceptButton = this.botonAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.botonCancelar;
+            this.ClientSize = new System.Drawing.Size(270, 115);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBoxLista);
+            this.Controls.Add(this.botonCancelar);
+            this.Controls.Add(this.botonAceptar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "IngresarNombreDeLista";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Guardar Lista de Reproducción";
+            this.TopMost = true;
+            this.groupBoxLista.ResumeLayout(false);
+            this.groupBoxLista.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonAceptar;
+        private System.Windows.Forms.Button botonCancelar;
+        private System.Windows.Forms.GroupBox groupBoxLista;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
